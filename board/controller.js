8556cb3bfd4f59454a902b550750fc01fc85cc59
unenglishable <unenglishable@gmail.com>
2014-10-30 19:38:29 -1000

    add board controller

---
+++ board/controller.js
@@ -0,0 +1,13 @@
+var controller = module.exports = {};
+
+// On put/del
+controller.onPut = function(key) {
+  // Initialize new metadata for the key from model
+};
+controller.onDel = function(key) {
+  // Delete the metadata for the key
+};
+
+controller.get = function(key) {
+  // return metadata for key
+};
