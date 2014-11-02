efa727deae481da78f58a4c25386c0f1927e3e32
unenglishable <unenglishable@gmail.com>
2014-11-01 17:43:30 -1000

    remove post controller
    onPut and onDel are handled in index

--- post/index.js
+++ post/index.js
@@ -1,4 +1,24 @@
 var path = require('path');
+var Operations = require(path.join(__dirname, '..', 'operations'));
 module.exports = {
-  controller: require(path.join(__dirname, 'controller'))
+  onPut: function(key) {
+    // Get parent thread
+    // Call increment on parentKey
+    var options = {
+      key: parentKey,
+      field: 'postCount',
+      recursive: true
+    };
+    Operations.increment(options);
+  },
+  onDel: function(key) {
+    // Get parent thread
+    // Call decrement on parentKey
+    var options = {
+      key: parentKey,
+      field: 'postCount',
+      recursive: true
+    };
+    Operations.decrement(options);
+  }
 };
