dfd2384a3493b7013ec7e85eb6f35dd8a6b1d407
unenglishable <unenglishable@gmail.com>
2014-11-01 17:42:34 -1000

    remove controller from board index

--- board/index.js
+++ board/index.js
@@ -1,5 +1,4 @@
 var path = require('path');
 module.exports = {
-  model: require(path.join(__dirname, 'model')),
-  controller: require(path.join(__dirname, 'controller'))
+  model: require(path.join(__dirname, 'model'))
 };
