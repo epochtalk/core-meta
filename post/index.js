6d4305f67dd42d6d9546cf72ee58ad82df403aba
unenglishable <unenglishable@gmail.com>
2014-11-03 16:32:24 -1000

    use Operations.getParent in post

--- post/index.js
+++ post/index.js
@@ -2,23 +2,23 @@ var path = require('path');
 var Operations = require(path.join(__dirname, '..', 'operations'));
 module.exports = {
   onPut: function(key) {
-    // Get parent thread
-    // Call increment on parentKey
-    var options = {
-      key: parentKey,
-      field: 'postCount',
-      recursive: true
-    };
-    Operations.increment(options);
+    Operations.getParentKey({key: key, callback: function(parentKey) {
+      // Call increment on parentKey's postCount
+      Operations.increment({
+        key: parentKey,
+        field: 'postCount',
+        recursive: true
+      });
+    }});
   },
-  onDel: function(key) {
-    // Get parent thread
-    // Call decrement on parentKey
-    var options = {
-      key: parentKey,
-      field: 'postCount',
-      recursive: true
-    };
-    Operations.decrement(options);
+  onDel: function(key, tree) {
+    Operations.getParentKey({key: key, callback: function(parentKey) {
+      // Call decrement on parentKey's postCount
+      Operations.decrement({
+          key: parentKey,
+          field: 'postCount',
+          recursive: true
+      });
+    }});
   }
 };
