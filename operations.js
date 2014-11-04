3927ea326cf2545da5a4ea5374d81e3ae42cdbdf
unenglishable <unenglishable@gmail.com>
2014-11-03 16:31:33 -1000

    implement operations

--- operations.js
+++ operations.js
@@ -1,4 +1,60 @@
 var Operations = module.exports = {};
+var tree;
+
+Operations.init(tree) {
+  tree = tree;
+};
+
+Operations.getParentKey(options) {
+  var q = {gt: options.key.concat(null), lt: options.key.concat(undefined), limit: 1};
+  var parentKey = null;
+  tree.roots.createReadStream(q).on('data', function(ch) {
+    parentKey = [ch.key[2], ch.key[3]];
+  }).on('end', function() {
+    options.callback(parentKey);
+  });
+};
+
+// options: key, callback
+Operations.getValue(options) {
+  var q = {gt: options.key.concat(null), lt: options.key.concat(undefined), limit: 1};
+  var value = null;
+  tree.roots.createReadStream(q).on('data', function(ch) {
+    // Get the original value
+    value = ch.value;
+  }).on('end', function() {
+    options.callback(value);
+  });
+};
+
+Operations.updateValue(options) {
+  var q = {gt: options.key.concat(null), lt: options.key.concat(undefined), limit: 1};
+  var value = null;
+  tree.roots.createReadStream(q).on('data', function(ch) {
+    // Get the original value
+    value = ch.value;
+  }).on('end', function() {
+    // Update the value
+    options.update({value: value, callback: function(){
+      // Put the new value to the key
+      var rows = [];
+      rows.push({type: 'put', key: key, value: value});
+      self.tree.meta.batch(rows, function() {
+        // Recurse if necessary
+        if (options.recursive) {
+          // Get parentKey
+          Operations.getParentKey({key: key, callback: function(parentKey) {
+            // Recurse
+            if (parentKey) {
+              options.key = parentKey;
+              Operations.add(options);
+            }
+          }});
+        }
+      });
+    }});
+  });
+};
 
 // options: key, field, recursive, callback
 Operations.increment = function(options) {
@@ -15,16 +71,10 @@ Operations.decrement = function(options) {
 // options: key, field, increment, recursive, callback
 Operations.add = function(options) {
   // Lock
-  // Get current value for key.field
-  // Add to the value
-  value += options.increment;
-  // Put the new value to the key
-  // Recurse if necessary
-  if (options.recursive) {
-    // Get parentKey
-    // Recurse
-    options.key = parentKey;
-    Operations.increment(options);
-  }
+  Operations.updateValue({key: options.key, update: function(updateOptions) {
+    // Add to the value
+    updateOptions.value[options.field] += options.increment;
+    updateOptions.callback(updateOptions.value);
+  }});
   // Release lock
 };
