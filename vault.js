098c8a5c2406788271223cea665ca1dde4cebab0
unenglishable <unenglishable@gmail.com>
2014-11-07 18:03:37 -1000

    use || in vault instead of ternary

--- vault.js
+++ vault.js
@@ -1,3 +1,3 @@
 var a = require('padlock').Padlock;
 var b = {};
-module.exports={getLock:function(c){b[c]=b[c]?b[c]:new a();return b[c];}}
+module.exports={getLock:function(c){b[c]=b[c]||new a();return b[c];}}
