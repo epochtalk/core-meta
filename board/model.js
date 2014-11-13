780c1dac3e8b8393c0551a799f18a17d58808c2c
Bronson Oka <boka@slickage.com>
2014-11-12 17:38:36 -1000

    update board model to match required scheme

--- board/model.js
+++ board/model.js
@@ -1,7 +1,11 @@
 module.exports = function() {
   // field = default
-  this.firstPost = null;
-  this.lastPost = null;
-  this.postCount = 0;
-  this.threadCount = 0;
+  this.post_count = 0;
+  this.thread_count = 0;
+  this.total_post_count = 0;
+  this.total_thread_count = 0;
+  this.last_post_username = '';
+  this.last_post_created_at = 0;
+  this.last_thread_title = '';
+  this.last_thread_id = '';
 };
