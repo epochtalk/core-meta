564b228c67064092202704237492bb645a762753
James Wang <jw@slickage.com>
2014-12-04 11:38:16 -0800

    Metadata layer now intended to not run as trigger.

--- post/index.js
+++ post/index.js
@@ -2,12 +2,19 @@ var path = require('path');
 var Operations = require(path.join(__dirname, '..', 'operations'));
 module.exports = {
   onPut: function(options) {
+    // use to query for post's user
+    // options.value.user_id;
     Operations.getParentKey({key: options.key, callback: function(parentKey) {
+      // Handle filling in parent thread data on first post
+      // THREAD UPDATE:
+      //   RECURSIVE: first_post_id, title, username, post_count,updated_at (iff post.created_at is later than the current udpdate_at)
+
       // Call increment on parentKey's postCount
       Operations.increment({
         key: parentKey,
         field: 'post_count',
-        recursive: true
+        recursive: true,
+        callback: options.callback
       });
     }});
   },
--- thread/model.js
+++ thread/model.js
@@ -2,5 +2,12 @@ module.exports = function() {
   // field = default
   this.first_post = null;
   this.last_post = null;
+  this.view_count = 0;
   this.post_count = 0;
+  this.last_post_username = '';
+  this.last_post_created_at = '';
+  // First post
+  this.title = '';
+  this.user = {username: ''};
+  this.first_post_id = '';
 };
